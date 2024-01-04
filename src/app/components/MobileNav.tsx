2eddfd6761c8aae9fb998098a0db0ce2dc321c9f
Saliha <Saliha@DESKTOP-FM767RQ>
2024-01-04 23:34:30 +0800

    Creating mobile nav

---
+++ src/app/components/MobileNav.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+
+const MobileNav = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="md:hidden p-2 pr-7 mt-7 m-2 text-lg flex flex-row-reverse items-start">
+
+        {/* Change menu button -> icon */}
+      <button onClick={toggleMenu}>|||</button>             
+      {isMenuOpen && (
+        <nav className="w-full space-y-4 flex flex-col items-start">
+          <Link href="/about" className="hover:text-teal-500">
+            About
+          </Link>
+          <Link href="/skills" className="hover:text-teal-500">
+            Skills
+          </Link>
+          <Link href="/projects" className="hover:text-teal-500">
+            Projects
+          </Link>
+        </nav>
+      )}
+      
+    </div>
+  );
+};
+
+export default MobileNav;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import DesktopNav from "./components/DesktopNav";
+import MobileNav from "./components/MobileNav";
 
 export default function Home() {
   return (
     <div>
       <DesktopNav></DesktopNav>
+      <MobileNav></MobileNav>
       <h1>Home page</h1>
     </div>
   );
