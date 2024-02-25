5b6b6eba9c97f6d7b9fdf337058fbe66668cece1
Saliha <saliha.work@gmail.com>
2024-02-25 13:59:07 +0800

    Add new project

--- src/app/lib/portfolio-data.ts
+++ src/app/lib/portfolio-data.ts
@@ -57,15 +57,20 @@ const portfolioData: PortfolioData = {
       id: 1,
       title: 'Online Banking System',
       description: "As a personal project, I developed Online Banking System using Spring Boot and PostgreSQL. The project focuses for managing bank accounts online. I implemented features such as user authentication, account creation, fund transfer, and account close. Thoroughly tested using Postman.",
-      githubLink: 'https://github.com/salihaaaaa/online-banking-system.git'
+      githubLink: 'https://github.com/salihaaaaa/Online-Banking-System.git'
     },
     {
       id: 2,
+      title: 'Customer Information API',
+      description: "Developed as a personal project, the Customer Information API is a RESTful API designed to manage customer data. Built using Spring Boot and PostgreSQL, the project focuses on implementing CRUD (Create, Read, Update, Delete) operations for customer information. The API enables the creation of new customers, reading customer details, updating existing customer information, and deleting customer records. The application is thoroughly tested using Postman to ensure reliability and functionality of the endpoints.",
+      githubLink: 'https://github.com/salihaaaaa/Customer-Information-API.git'
+    },
+    {
+      id: 3,
       title: 'Portfolio',
       description: 'A portfolio website that reflects my journey in self-learning. Utilizing the power of Next.js and the simplicity of Tailwind CSS, this website serves as a testament to my dedication to mastering new technologies. Through this project, I honed my skills in building dynamic and responsive web applications while creating an interactive space for presenting my achievements and capabilities.',
       githubLink: 'https://github.com/salihaaaaa/portfolio.git'
     }
-    
   ],
 };
 
